1dbc56136ae073fa1204f629696d592ee08b4dda
hoge <2104240033yt@edu.oca.ac.jp>
2023-05-13 15:26:55 +0900

    aaaa

--- test.py
+++ test.py
@@ -1,5 +1,18 @@
+<<<<<<< HEAD
 def main():
     print('うんちいいいいいいいいいいい！')
 
 if __name__=='__main__':
     main()
+=======
+numbers = [1, 2, 4, 7]
+for number in numbers:
+    mod = number % 2
+    if mod == 0:
+        print('偶数なのでbreakします')
+        break
+    else:
+        print(str(number) + 'は奇数')
+else:
+    print('全て奇数でした')
+>>>>>>> 6aab209 (ifroop)
